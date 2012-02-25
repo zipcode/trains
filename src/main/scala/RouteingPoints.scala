fc9aa2d8d0d126c2e7c8fb293756e336e797014c
@voqo <->
2012-02-25 01:14:39 +0000

    Routeing point parser

--- src/main/scala/RouteingPoints.scala
+++ src/main/scala/RouteingPoints.scala
@@ -14,7 +14,9 @@ object RouteingPoints {
       Log.error("Wrong number of arguments")
       System.exit(1)
     }
-    extract(args(0))
+
+    val rps = parse(extract(args(0)))
+    rps.rps.map { println(_) }
   }
 
   /**
@@ -35,6 +37,55 @@ object RouteingPoints {
     output.flush
     stream.toString("UTF-8").split(sep).map { _.split("\n").drop(4) }.flatten.toList
   }
+
+  def parse(lines: Seq[String]): RouteingPoints = {
+    val rps = new RouteingPoints()
+
+    val points = lines collect { case RouteingOrGroupPoint(s, p) => p } toSet
+
+    (lines collect {
+      case RouteingOrGroupPoint(s, p) => List((s, p))
+      case line => parseLine(points, line)
+    }).flatten.foreach { t => rps addRP (t._1, t._2) }
+
+    rps
+  }
+
+  def parseLine(points: Set[String], line: String): List[(String,String)] = {
+    val tokens: List[String] = unfold(line.toLowerCase) { ss =>
+      points find (ss endsWith _) match {
+        case Some(s) => Some(s, ss.substring(0, ss.size - s.size - 1))
+        case None => if (ss.size == 0) None else Some(ss, "")
+      }
+    }
+    tokens.reverse.tail map { tokens.reverse.head -> _ }
+  }
+
+  def unfold[S,T](init: T)(f: T => Option[(S, T)]): List[S] = f(init) match {
+    case Some((item, remain)) => item :: unfold(remain)(f)
+    case None => Nil
+  }
 }
 
-class RouteingPoints
+class RouteingPoints {
+  import scala.collection.{mutable => m}
+  val rps = new m.HashMap[String, m.Set[String]] with m.MultiMap[String, String]
+
+  def apply = rps.apply _
+  def addRP = rps.addBinding _
+}
+
+object RouteingOrGroupPoint {
+  def unapply(s: String): Option[(String, String)] = {
+    import scala.util.matching.Regex.Match
+
+    val matchers = List(("^(.*) Routeing Point$", 0, 0),
+                        ("^([^a-z ]+( [^a-z ]+)*) (.*) Routeing Point Member$", 0, 2))
+    (matchers map { case (pattern, k1, k2) =>
+      pattern.r.findFirstMatchIn(s).map { m: Match =>
+        val f = { n: Int => m.subgroups(n).toLowerCase }
+        (f(k1), f(k2))
+      }
+    }) find (_.isDefined) map (_.get)
+  }
+}
