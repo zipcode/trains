c3adda79fde66eea75b6cddc38b8cabd15ba79c0
@voqo <->
2012-04-09 15:07:23 +0100

    Hopefully correctly extracting everything

---
+++ src/main/scala/PermittedRouteIdentifier.scala
@@ -0,0 +1,42 @@
+package com.github.voqo.train
+
+import java.io.File
+
+object PermittedRouteIdentifier {
+  val targetfile = "permitted_route_identifier.pdf"
+
+  def fromDirectory(s: String, p: Set[String]) = fromPDF(new File(s, targetfile), p)
+
+  def fromPDF(f: File, p: Set[String]) = {
+    parse(PDFReader.getPages(f) flatMap { _.split("\n").drop(1) }, p)
+  }
+
+  private def parse(lines: Seq[String], points: Set[String]): Seq[(String, String, Seq[String])] = {
+    val splitlines = for (line <- lines) yield {
+      /* Chomp known routeing points off the start. */
+      val tokens = Util.unfold(line) { ss =>
+        points find {p: String => ss.toLowerCase startsWith (p.toLowerCase + " ")} match {
+          case Some(s) => Some(s, ss drop (s.size + 1))
+          case None => None
+        }
+      }
+      /* What remains will be maps */
+      (tokens, line drop (tokens.mkString(" ", "", " ").size))
+    }
+
+    /* Some things are split onto new lines. Tack 'em on */
+    val entries = splitlines.foldRight(List[(Seq[String], String)]()) { (item, acc) =>
+      if (acc.size > 0 && acc.head._1 == Nil) {
+        (item._1 -> (item._2 ++ " " ++ acc.head._2)) :: acc.tail
+      } else {
+        item :: acc
+      }
+    }
+
+    /* Fix it up to be the right datatype */
+    entries map { case (stations, points) =>
+      val List(p1, p2) = stations
+      (p1, p2, points.split(" ").toSeq)
+    }       
+  }
+}
--- src/main/scala/Router.scala
+++ src/main/scala/Router.scala
@@ -9,8 +9,11 @@ object Router {
 
     val routeing_points = RouteingPoints.fromDirectory(args(0))
     val routeing_point_identifier = RouteingPointIdentifier.fromDirectory(args(0))
+    val points = routeing_point_identifier.map(_._2).flatten.toSet
+    val permitted_routes = PermittedRouteIdentifier.fromDirectory(args(0), points)
 
     routeing_points foreach { case (p, m) => println("Map => %s: %s".format(p, m.mkString(", "))) }
     routeing_point_identifier foreach { case (p, m) => println("Point => %s: %s".format(p, m.mkString(", "))) }
+    permitted_routes foreach { case (p1, p2, ms) => println("Permitted route => %s - %s: %s".format(p1, p2, ms.mkString(" "))) }
   }
 }
