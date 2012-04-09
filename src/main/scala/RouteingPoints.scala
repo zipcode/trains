6de45adddcba6ce57aa7f7b1c242469b35556e70
@voqo <->
2012-04-09 13:35:31 +0100

    Fixed newline bug in RouteingPoints

--- src/main/scala/RouteingPoints.scala
+++ src/main/scala/RouteingPoints.scala
@@ -13,13 +13,13 @@ object RouteingPoints {
 
   /**
    * Extract Routeing Point to Map mappings
-   *
-   * This relies upon each Map name being two characters, and the map names being
-   * listed in alphabetical order. Thus we can chomp off two-character names as long
-   * as they're ascending and thus avoid capturing the JN on the end of WILLESDEN JN.
+   * This relies upon each Map name being two characters, and the map
+   * names being listed in alphabetical order. Thus we can chomp off
+   * two-character names as long as they're ascending and avoid capturing
+   * the JN on the end of WILLESDEN JN.
    */
   private def parse(lines: Seq[String]) = {
-    for (line <- lines) yield {
+    val entries = for (line <- lines) yield {
       val tokens = line split " "
       val ms: List[String] = tokens.foldRight(List[String]()) { (token, acc) =>
         if (token.size == 2 && (acc.isEmpty || token < acc.head)) {
@@ -28,6 +28,15 @@ object RouteingPoints {
       }
       val s = tokens take (tokens.size - ms.size) mkString " "
       (s, ms)
+    }
+
+    /* One or two entries roll onto the next line.  Fix it up.*/
+    entries.foldRight(List[(String, Seq[String])]()) { (item, acc) =>
+      if (acc.size > 0 && acc.head._1 == "") {
+        (item._1 -> (item._2 ++ acc.head._2)) :: acc.tail
+      } else {
+        item :: acc
+      }
     }
   }
 }
